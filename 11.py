dd32862f675067ef2505b8e21a965cf09dc6f493
GaganNaagu <GaganSG1908@gmail.com>
2026-06-24 12:37:32 +0530

    refactor: inline bias variables in perceptron logic for AND/OR gates

--- 11.py
+++ 11.py
@@ -11,15 +11,13 @@ inputs = np.array([[0,0], [0,1], [1,0], [1,1]])
 
 print("AND Gate")
 weights = np.array([1, 1])
-bias_and = -1.5
 
 for x in inputs:
-    output = perceptron(x, weights, bias_and)
+    output = perceptron(x, weights, -1.5)
     print(f"Input: {x} -> Output: {output}")
 
 print("\nOR Gate")
-bias_or = -0.5
 
 for x in inputs:
-    output = perceptron(x, weights, bias_or)
+    output = perceptron(x, weights, -0.5)
     print(f"Input: {x} -> Output: {output}")
